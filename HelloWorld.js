2a852801049da481c2443ae5fe47c6b21bc14006
Ankita Kumari <ankita00iv@gmail.com>
2023-09-21 13:35:52 +0530

    initial commit

---
+++ HelloWorld.js
@@ -0,0 +1,5 @@
+console.log("hello world..!")
+a = "Everything OK...✨?"
+console.log(a)
+a = 0
+console.log(a)
---
+++ PrimitiveDatatypes.js
@@ -0,0 +1,10 @@
+//nn bb ss u
+let a = null
+let b = 11
+let c = true
+let d = BigInt("22") + BigInt("8")
+let e = "annie"
+let f = Symbol("I am a nice symbol")
+let g
+console.log(a, b, c, d, e, f, g)
+console.log(typeof d)
