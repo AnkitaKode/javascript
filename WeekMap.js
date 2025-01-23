49554dd09975e932e8bf1abe11cd389c3f57e61a
Ankita Kumari <ankita00iv@gmail.com>
2025-01-23 20:04:49 +0530

    initial commit

---
+++ WeekMap.js
@@ -0,0 +1,12 @@
+function weekmap() {
+    let looseMap = new WeakMap();
+    let Ram = {name};
+    let Raj = {name};
+    let Rahul = {name};
+    looseMap.set(Ram, "Ram");
+    looseMap.set(Raj, "Raj");
+    looseMap.set(Rahul, "Rahul");
+    console.log(looseMap);
+    console.log(looseMap.has(Ram))
+}
+weekMap();
